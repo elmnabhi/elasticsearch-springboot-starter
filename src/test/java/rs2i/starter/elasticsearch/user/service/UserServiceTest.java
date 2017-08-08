9500f51ec0c7c818c3113558c1c33675a4a00745
Merouane El Mnabhi <melmnabhi@rs2i.fr>
2017-08-08 16:58:18 +0200

    UserServiceTest

---
+++ src/test/java/rs2i/starter/elasticsearch/user/service/UserServiceTest.java
@@ -0,0 +1,53 @@
+package rs2i.starter.elasticsearch.user.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import rs2i.starter.elasticsearch.common.exceptions.EntityNotFoundException;
+import rs2i.starter.elasticsearch.user.domain.User;
+import rs2i.starter.elasticsearch.user.repository.UserRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    UserService userService = new UserServiceImpl();
+
+    @Mock
+    UserRepository userRepository;
+
+    @Test(expected = EntityNotFoundException.class)
+    public void findByUserNameThrowEntityNotFoundExceptionWhenUserIsNotFound() throws EntityNotFoundException {
+
+        //GIVEN
+        String userName = "userName";
+        Mockito.when(userRepository.findByUserName(userName)).thenReturn(null);
+
+        //WHEN
+        User user = userService.findByUserName(userName);
+
+        //THEN
+        Assert.fail();
+    }
+
+    @Test
+    public void findByUserNameReturnsTheRightUser() throws EntityNotFoundException {
+
+        //GIVEN
+        String userName = "Merouane";
+        User expectedUser = User.builder().password("TYU7865H").id("User::2").userName("Merouane").build();
+        Mockito.when(userRepository.findByUserName(userName)).thenReturn(expectedUser);
+
+        //WHEN
+        User user = userService.findByUserName(userName);
+
+        //THEN
+        assertThat(user).isEqualTo(expectedUser);
+    }
+}
